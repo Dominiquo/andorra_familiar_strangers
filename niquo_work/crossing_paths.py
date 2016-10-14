8d36bbc5e824c9f4a6e4fb805cae8d0ab2df794d
Dominiquo Santistevan <niquo@mit.edu>
2016-10-14 01:56:39 -0400

    minimal changes made to help logging

--- niquo_work/crossing_paths.py
+++ niquo_work/crossing_paths.py
@@ -51,18 +51,19 @@ def create_pair_users_obj(towers_directory,destination_path,file_limit=float('in
 
 def pair_users_from_towers(towers_directory,limit = float('inf')):
 	all_tower_files = set(os.listdir(towers_directory))
-	"total count of available tower files:",len(all_tower_files)
+	print "total count of available tower files:",len(all_tower_files)
 	inf = float('inf')
 	pair_map = {}
 	current = 0
 	for tower_name in all_tower_files:
-		"checking file", tower_name,"for users that met..."
+		print "checking file", tower_name,"for users that met..."
 		if current > limit:
 			break
 		tower_path = towers_directory + tower_name
 		all_callers = ex.read_csv(tower_path,inf)
+		print len(all_callers),"callers found in file:",tower_name
 		pairs = find_collisions_from_tower(all_callers)
-		"found",len(pairs),"of callers that met"
+		print "found",len(pairs),"of callers that met"
 		for first,second in pairs:
 			first_number = first[CALLER_INDEX]
 			second_number = second[CALLER_INDEX]
@@ -118,6 +119,6 @@ def main():
 	# print "partitioning complete"
 	towers_directory = '../niquo_data/partitioned_towers/'
 	destination_path = '../niquo_data/paired_callers/paired_dict.p'
-	create_pair_users_obj(towers_directory,destination_path,file_limit=2)
+	create_pair_users_obj(towers_directory,destination_path,file_limit=100)
 
 main()
