1d18d8aa1c59138f7b1e0f076d7aaeed8ef58f81
Niquo <niquo@mit.edu>
2016-11-13 13:09:14 -0500

    encounters file writer now includes towers and first encounter

--- niquo_work/crossing_paths.py
+++ niquo_work/crossing_paths.py
@@ -313,6 +313,8 @@ def find_mult_enc_single_week(week_path,destination_path,n=2):
 		tower_count += 1
 		tower_path = week_path + tower
 		all_maps[tower] = cPickle.load(open(tower_path,'rb'))
+		if tower_count > 3:
+			break
 
 	print 'loading files complete..'
 	for tower, tower_enc_map in all_maps.iteritems():
@@ -323,10 +325,11 @@ def find_mult_enc_single_week(week_path,destination_path,n=2):
 				if caller_enc == caller or (len(times) < n-1):
 					continue
 				last_time = times[-1]
-				delta_days, delta_seconds = find_next_encounter(tower,caller,caller_enc,last_time,all_maps)
+				delta_days, delta_seconds, next_tower = find_next_encounter(tower,caller,caller_enc,last_time,all_maps)
 				if not delta_days:
 					continue
-				row = [caller,caller_enc,delta_days,delta_seconds]
+				# ROW STRUCTURE OF ENCOUNTERS
+				row = [caller,caller_enc,delta_days,delta_seconds,tower,next_tower,last_time]
 				print 'found encounter..'
 				encounter_times_csv.writerow(row)
 	return True
@@ -340,19 +343,19 @@ def find_next_encounter(tower,caller,caller_enc,last_time,all_maps):
 		if (caller in enc_map) and (caller_enc in enc_map[caller]):
 			last_enc = find_nearest_time(enc_map[caller][caller_enc],last_time)
 			if last_time != None:
-				most_recent.append(last_enc)
+				most_recent.append((last_enc,t))
 
 		elif (caller_enc in enc_map) and (caller in enc_map[caller_enc]):
 			last_enc = find_nearest_time(enc_map[caller_enc][caller],last_time)
 			if last_enc != None:
-				most_recent.append(last_enc)
+				most_recent.append((last_enc,t))
 
-	if most_recent == []:
-		return None,None
-	elif min(most_recent) != None:
-		closest_encounter = min(most_recent)
-		return time_difference(last_time,closest_encounter)
-	return None,None
+	if min(most_recent,key=lambda x:x[0]) != None:
+		closest_encounter, next_tower = min(most_recent,key=lambda x:x[0])
+		delta_days, delta_seconds = time_difference(last_time,closest_encounter)
+		return delta_days, delta_seconds, next_tower
+	else:
+		return None, None, None
 
 
 def find_nearest_time(encs_list,last_encounter):
@@ -367,6 +370,10 @@ def find_nearest_time(encs_list,last_encounter):
 				print 'found time after search:',min_time_met
 				return min_time_met
 	return None
+
+
+def encounter_after_threshold(time,hour):
+	return True
 
 
 # ********************************************** 
