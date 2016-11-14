5491079cf1b04d480bc573c7bdd8cfb606c071ac
Niquo <niquo@mit.edu>
2016-11-14 11:32:20 -0500

    simple histogram creation for files

--- niquo_work/analyze_connections.py
+++ niquo_work/analyze_connections.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import crossing_paths as cp
+import extractData as ex
 
 def get_encounters_count(enc_map):
 	all_counts = []
@@ -29,9 +30,26 @@ def get_entire_distribution(enc_maps_path):
 			all_encounters_count.append(get_encounters_count(enc_map))
 	return np.array(all_encounters_count)
 
+def filter_produce_hist(file_path,save_path,filter_func=lambda x: True):
+	days_row = 2
+	all_rows = ex.read_csv(file_path,float('inf'))
+	x_vals = [row[days_row] for row in all_rows if filter_func(row)]
+
+	filename =  file_path.split('/')[-1].split('.')[0] + '.png'
+	create_dist_histogram(x_vals,save_path + filename)
+	return True
+
 def create_dist_histogram(x_vals,save_file):
 	plt.hist(x_vals)
 	plt.savefig(save_file)
 	return True
+
+def first_encountered_after_hour():
+	return True
+
+def encountered_on_weekends():
+	return True
+
+
 
 
