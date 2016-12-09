594a756f09ea28804b9685d4525f4a24025a7eed
Niquo <niquo@mit.edu>
2016-12-09 02:03:50 -0500

    make the dirs

--- niquo_work/Main.py
+++ niquo_work/Main.py
@@ -18,6 +18,7 @@ def Main(root_path='../niquo_data/v2_data_root', data_path='../niquo_data/filter
 	towers_dir_name = 'partitioned_towers'
 	towers_path = os.path.join(root_path, towers_dir_name)
 	if not os.path.exists(towers_path):
+				print 'made directory', towers_path
 				os.makedirs(towers_path)
 	print 'partitioning raw data in tower files to be stored in directory: ', towers_path
 	csvData = raw.RawCDRCSV(data_path)
