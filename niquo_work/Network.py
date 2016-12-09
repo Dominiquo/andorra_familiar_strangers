b4f60e1c4f00cd7e5eeb1dee26961ace0359d894
Niquo <niquo@mit.edu>
2016-12-09 16:46:39 -0500

    removed print statements

--- niquo_work/Network.py
+++ niquo_work/Network.py
@@ -48,7 +48,6 @@ def get_distribution_encounters(encoutners_csv,network_object_path,destination_p
 				if (caller in nodes_set) and (encounteree in nodes_set):
 					try:
 						distance = nx.shortest_path_length(friend_graph, source=caller, target=encounteree)
-						print 'found two users that are at a distance of', distance
 						row.append(distance)
 						distance_csv.writerow(row)
 					except Exception as e:
