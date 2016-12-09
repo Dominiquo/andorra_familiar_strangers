dc46db08b2b40258a3cd2ce84f3e591e96a6a0c6
Niquo <niquo@mit.edu>
2016-12-09 15:47:01 -0500

    defined prefix

--- niquo_work/Main.py
+++ niquo_work/Main.py
@@ -9,7 +9,7 @@ def create_encounter(data_path, dest_path, n_vals):
 	# TODO: take off indexing to do all weeks
 	week_paths = sorted(os.listdir(data_path))[0]
 	for week_path in week_paths:
-			full_path = os.path.join(prefix, week_path)
+			full_path = os.path.join(data_path, week_path)
 			for n in range(2,20,4):
 				dest_base = '%s_encounter_n_%s.csv' % (week_path, n)
 				full_destination = os.path.join(dest_base,dest_base)
