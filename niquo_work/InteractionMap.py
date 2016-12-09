9a21e000a7502fd13d68333115376675b1ce9d66
Niquo <niquo@mit.edu>
2016-12-08 20:13:51 -0500

    semantic errors fixed

--- niquo_work/InteractionMap.py
+++ niquo_work/InteractionMap.py
@@ -47,7 +47,7 @@ class InteractionMap(object):
 
 	@classmethod
 	def createInteractionMapsSet(cls,days_directory):
-		return [InteractionMap(path) for path in os.listdir(days_directory)]
+		return [InteractionMap(os.path.join(days_directory, path)) for path in os.listdir(days_directory)]
 
 
 
