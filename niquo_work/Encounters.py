dde0cad2bf25b43c6f21f165c6042f5ef95fbe92
Niquo <niquo@mit.edu>
2016-12-11 16:28:31 -0500

    write encounters to json file, store all initial encounters data, do not skip if met on first day

--- niquo_work/Encounters.py
+++ niquo_work/Encounters.py
@@ -2,6 +2,7 @@ import csv
 import os
 import cPickle
 import itertools
+import encounters_analysis as encs_anal
 from datetime import datetime
 import time
 
@@ -12,7 +13,6 @@ def find_mult_enc_single_week(week_path,destination_path,n=2):
 	all_towers = os.listdir(week_path)
 	print 'checking', len(all_towers),'total tower files...'
 	all_maps = {}
-	encounter_times_csv = csv.writer(destination_file,delimiter=';')
 	print 'openend', destination_file,'for writing csv data...'
 	print 'loading tower files in to RAM..'
 	tower_count = 1
@@ -21,24 +21,31 @@ def find_mult_enc_single_week(week_path,destination_path,n=2):
 		tower_count += 1
 		tower_path = os.path.join(week_path, tower)
 		all_maps[tower] = cPickle.load(open(tower_path,'rb'))
-
 	print 'loading files complete..'
-	for tower, tower_enc_map in all_maps.iteritems():
-		print 'checking matches for tower:',tower
-		print tower,'contains',len(tower_enc_map),'encounterees'
-		for caller, encounteree_map in tower_enc_map.iteritems():
-			for caller_enc, times in encounteree_map.iteritems():
-				# if len(times) != n:
-					# continue
-				if caller_enc == caller or (len(times) < n-1):
-					continue
-				last_time = times[-1]
-				delta_days, delta_seconds, next_tower = find_next_encounter(tower,caller,caller_enc,last_time,all_maps)
-				if not delta_days:
-					continue
-				# ROW STRUCTURE OF ENCOUNTERS
-				row = [caller,caller_enc,delta_days,delta_seconds,tower,next_tower,last_time]
-				encounter_times_csv.writerow(row)
+	with open(destination_file,'wb') as json_out:
+		for tower, tower_enc_map in all_maps.iteritems():
+			print 'checking matches for tower:',tower
+			print tower,'contains',len(tower_enc_map),'encounterees'
+			for caller, encounteree_map in tower_enc_map.iteritems():
+				for caller_enc, times in encounteree_map.iteritems():
+					# if len(times) != n:
+						# continue
+					if caller_enc == caller or (len(times) != n):
+						continue
+					times.sort()
+					last_time = times[-1]
+					delta_days, delta_seconds, next_tower = find_next_encounter(tower,caller,caller_enc,last_time,all_maps)
+					next_time = encs_anal.create_time_string_from_delta(last_time,delta_days,delta_seconds)
+					row = {'caller': caller,
+							'caller_enc': caller_enc,
+							'first_times:': times
+							'next_time': next_time,
+							'delta_days': delta_days,
+							'delta_seconds': delta_seconds
+							'first_tower': tower,
+							'next_tower': next_tower}
+					json.dump(row,json_out)
+					json_out.write('\n')
 	return True
 
 def find_next_encounter(tower,caller,caller_enc,last_time,all_maps):
@@ -66,15 +73,13 @@ def find_next_encounter(tower,caller,caller_enc,last_time,all_maps):
 
 
 def find_nearest_time(encs_list,last_encounter):
+	encs_list.sort()
 	min_time_met = encs_list[0]
 	if min_time_met > last_encounter:
-		# print 'found time:',min_time_met
 		return min_time_met
 	else:
-		# print 'met before first encounter, finding later encounter...'
 		for min_time_met in encs_list:
 			if min_time_met > last_encounter:
-				# print 'found time after search:',min_time_met
 				return min_time_met
 	return None
 
