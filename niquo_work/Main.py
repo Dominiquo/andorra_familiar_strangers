9ef29c07abc0c3d935342777fd7fa9520444ed3e
Niquo <niquo@mit.edu>
2016-12-11 19:52:47 -0500

    only run what I need ran; combining pairs and producing json files

--- niquo_work/Main.py
+++ niquo_work/Main.py
@@ -10,7 +10,7 @@ def create_encounter(data_path, dest_path, n_vals):
 	for week_path in week_paths:
 			full_path = os.path.join(data_path, week_path)
 			for n in range(2,20,4):
-				dest_base = '%s_encounter_n_%s.csv' % (week_path, n)
+				dest_base = '%s_encounter_n_%s.json' % (week_path, n)
 				full_destination = os.path.join(dest_base,dest_base)
 				encs.find_mult_enc_single_week(full_path,full_destination,n)
 
@@ -22,16 +22,16 @@ def Main(root_path='../niquo_data/v2_data_root', data_path='../niquo_data/filter
 				print 'made directory', towers_path
 				os.makedirs(towers_path)
 	print 'partitioning raw data in tower files to be stored in directory: ', towers_path
-	csvData = raw.RawCDRCSV(data_path)
-	csvData.filter_and_partition(towers_path)
+	# csvData = raw.RawCDRCSV(data_path)
+	# csvData.filter_and_partition(towers_path)
 
 	paired_dir_name = 'paired_callers'
 	paired_path = os.path.join(root_path,paired_dir_name)
 	print 'finding user call pairs to be stored ', paired_path
 	if not os.path.exists(paired_path):
 				os.makedirs(paired_path)
-	partitioned = TP.TowersPartitioned(towers_path)
-	partitioned.pair_users_from_towers(paired_path)
+	# partitioned = TP.TowersPartitioned(towers_path)
+	# partitioned.pair_users_from_towers(paired_path)
 
 	combo_dir_name = 'combined_callers'
 	combo_path = os.path.join(root_path,combo_dir_name)
