4dc425cd4c822c325698334cac031cb984a21367
Niquo <niquo@mit.edu>
2016-12-12 14:34:50 -0500

    index the row properly

--- niquo_work/encounters_analysis.py
+++ niquo_work/encounters_analysis.py
@@ -57,7 +57,7 @@ def create_times_filter_func(first_cond,second_cond, n, use_majority=True):
 		return lambda row: first_cond(row[first_times][last_element]) and second_cond(row[next_time]) and (len(row[first_times]) == n)
 
 def create_encounters_count_filter(n):
-	return lambda row: ((row['first_times'] > n) and (row['distance'] > 0)
+	return lambda row: ((row['first_times'] > n) and (row['distance'] > 0))
 
 def create_friend_dist_graph(encounters_json, destination_path, n):
 	filter_func = create_encounters_count_filter(n)
