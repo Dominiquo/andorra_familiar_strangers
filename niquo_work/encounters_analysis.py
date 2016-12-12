e0733530ba8a7db6fa33e6acb0dba912bdb4406a
Niquo <niquo@mit.edu>
2016-12-11 19:57:05 -0500

    WIP

--- niquo_work/encounters_analysis.py
+++ niquo_work/encounters_analysis.py
@@ -11,25 +11,43 @@ import itertools
 
 def read_json_file_generator(json_filename,limit=float('inf')):
 	with open(filename) as infile:
-	for line in infile:
-		yield json.loads(line)
+		for line in infile:
+			yield json.loads(line)
 
 def filter_xvals(json_filename,filter_func=lambda row:True):
 	return [convert_row(row) for row in read_json_file_generator(json_filename) if filter_func(row)]
 
 
-def create_graphs_on_tower_type(encounters_json, destination_path):
-	return True
+# def create_graphs_on_tower_type(encounters_json, destination_path):
+# 	return True
 
 def create_graphs_on_times(encounters_json, destination_path):
 	all_conditions = [isMorning,isHome,isNight]
-	
+	for first, second in itertools.permutations(all_conditions, 2):
+		filter_func = create_times_filter_func(first,second)
+		x_vals = filter_xvals
 
 	return True
 
-def create_friend_dist_graph(encounters_json, destination_path):
+# def create_loc_filter_func(first, second, location_map, use_majority=True):
 
-	return True
+# 	if use_majority:
+
+# 	else:
+# 		return 
+
+def create_times_filter_func(first_cond,second_cond, use_majority=True):
+	first_times = 'first_times'
+	next_time = 'next_time'
+	last_element = -1
+	if use_majority:
+		return lambda row: majority_check(row[first_times],first_cond) and second_cond(row[next_time])
+	else:
+		return lambda row: first_cond(row[first_times][last_element]) and second_cond(row[next_time])
+
+# def create_friend_dist_graph(encounters_json, destination_path):
+
+# 	return True
 
 def create_box_plot():
 	return True
@@ -41,8 +59,11 @@ def create_box_plot():
 
 def majority_check(all_times,filter_func):
 	n = len(all_times)
-
-
+	positive_vals = sum([1 for time_str in all_times if filter_func(time_str)])
+	if (n % 2) == 0:
+		return (positive_vals >= (n/2))
+	else:
+		return (positive_vals > (n/2))
 
 def isMorning(time):
 	return time_in_range(time,9,17)
