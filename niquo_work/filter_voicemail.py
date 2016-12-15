47cebc1f4ee4e4001e54196a24374e86270870ce
Niquo <niquo@mit.edu>
2016-12-15 13:51:16 -0500

    write all same times calls not just missed calls

--- niquo_work/filter_voicemail.py
+++ niquo_work/filter_voicemail.py
@@ -6,17 +6,46 @@ import itertools
 import RawCRDData as raw
 
 
-unfiltered_data = '../niquo_data/filtered_data/06_2017_no_data.csv'
+filtered_data = '../niquo_data/filtered_data/06_2017_no_data.csv'
 outgoing_only = '../niquo_data/filtered_data/outgoingonly_calls_june.csv'
 voicemail_map = '../niquo_data/filtered_data/voicemail_map.p'
+sorted_rows = '../niquo_data/filtered_data/sorted_rows.p'
 comm_type_index = 10
 caller_index = 0
 receiver_index = 16
 start_index = 3
 end_index = 4
 
+def walk_through_pairs(sorted_rows_file=sorted_rows):
+	all_values = cPickle.load(open(sorted_rows_file))
+	count = 0
+	with open(outgoing_only, 'wb') as outfile:
+		while (i+1) < len(all_values):
+			first = all_values[i]
+			second = all_values[i+1]
+			f_start = first[start_index]
+			f_end = first[end_index]
+			s_start = second[start_index]
+			s_end = second[end_index]
+			if (f_start == s_start) and (f_end == s_end):
+				count += 1
+				f_caller = first[caller_index]
+				f_receiver = first[receiver_index]
+				s_caller = second[caller_index]
+				s_reciever = second[receiver_index]
+				obj1 = {'caller': f_caller, 'receiver': f_receiver, 's_time': f_start, 'e_time': f_end}
+				obj2 = {'caller': s_caller, 'receiver': s_reciever, 's_time': s_start, 'e_time': s_end}
+				json.dump(obj1,outfile)
+				outfile.write('\n')
+				json.dump(obj2,outfile)
+				outfile.write('\n')
+	print 'wrote ', count, 'objects to file'
 
-def filter_calls_voicemail(csv_data=unfiltered_data):
+
+
+
+
+def filter_calls_voicemail(csv_data=filtered_data):
 	raw_data = raw.RawCDRCSV(csv_data)
 	count = 0
 	with open(outgoing_only, 'wb') as outfile:
