3daeda87ed0dd435c3a1172f674b69c2fd1a9080
Niquo <niquo@mit.edu>
2016-12-16 15:39:34 -0500

    create new json file of data with new values added

---
+++ niquo_work/update_encounters_json.py
@@ -0,0 +1,21 @@
+import json
+
+def add_element_json(old_json, new_json):
+	first_times = 'first_times'
+	count = 0
+	with open(old_json) as infile:
+		with open(new_json, 'wb') as outfile:
+			for line in infile:
+				old_val = json.loads(line)
+				times = old_val[first_times]
+				encs_count = get_mew_enc_count(times)
+				old_val['encs_count'] = encs_count
+				json.dump(old_val, outfile)
+				outfile.write('\n')
+	print 'wrote', count, 'new values to ', new_json
+
+
+def get_mew_enc_count(times_list):
+	hour_index_cutoff = 13
+	hours_set = set([t[:13] for t in times_list])
+	return len(hours_set)
