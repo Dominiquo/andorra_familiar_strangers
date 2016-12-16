3fb7b98c44c8cebdc8a15c02e554122b3dacc8a0
Niquo <niquo@mit.edu>
2016-12-16 17:11:17 -0500

    create graph when making csv

--- niquo_work/encounters_analysis.py
+++ niquo_work/encounters_analysis.py
@@ -158,8 +158,8 @@ def generate_stats_per_tower(encounters_json,save_file='../niquo_data/filtered_d
 			csvout.writerow([lat,lon,med,mean])
 
 	for source,dest in towers_graph.edges():
-		towers_graph[source][dest]['mean_hours'] = np.mean(towers_graph[source][dest]['times'])
-		towers_graph[source][dest]['med_hours'] = np.median(towers_graph[source][dest]['times'])
+		tower_graph[source][dest]['mean_hours'] = np.mean(towers_graph[source][dest]['times'])
+		tower_graph[source][dest]['med_hours'] = np.median(towers_graph[source][dest]['times'])
 
 	return towers_graph
 
