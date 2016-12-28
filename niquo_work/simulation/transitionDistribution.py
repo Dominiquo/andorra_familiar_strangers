c35e79a315b75153f6cc79646581083afee15814
Niquo <niquo@mit.edu>
2016-12-28 13:15:52 -0700

    added location distribution functions

--- niquo_work/simulation/transitionDistribution.py
+++ niquo_work/simulation/transitionDistribution.py
@@ -64,6 +64,7 @@ def get_minute_location_dist(data):
 	data['minute_start'] = data.apply(lambda row: row['DT_CDDATAINICI'][11:16], axis=1)
 	time_loc_dist = {}
 	for name, group in data.groupby('minute_start'):
-		time_loc_dist[name] = location_type_dist(group)
+		tower_dist = get_basic_tower_dist(group)
+		time_loc_dist[name] = location_type_dist(tower_dist)
 	return time_loc_dist
 
