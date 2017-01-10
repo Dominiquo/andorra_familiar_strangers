6a37fcc5967e14b9a5f9d5c6fec217de1ba9ad86
Niquo <niquo@mit.edu>
2017-01-10 22:04:16 +0100

    run simluation remotely on data that will soon be there

---
+++ niquo_work/simulation/run_simulation.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append("..")
+import cPickle
+import RawCRDData as raw
+import TowersPartitioned as TP
+import InteractionMap as imap
+import Encounters as encs
+import Network
+import Main as prev_main
+
+data_path = '../../niquo_data/July_first_week...csv'
+destination_path = '../../niquo_data/simulation'
+graph_path = '../../niquo_data/simulation/simulation_graph.p'
+
+
+Network.create_graph(data_path, graph_path)
+Network.clean_graph(graph_path, graph_path, limit=100)
+
+prev_main.Main(destination_path, data_path, graph_path)
+
+
