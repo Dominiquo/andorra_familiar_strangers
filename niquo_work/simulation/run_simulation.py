f81e495f3b3eb0831e3fe07d18092fa40b7e73a8
Niquo <niquo@mit.edu>
2017-01-10 22:05:26 +0100

    run simluation remotely on data that will soon be there

--- niquo_work/simulation/run_simulation.py
+++ niquo_work/simulation/run_simulation.py
@@ -8,7 +8,7 @@ import Encounters as encs
 import Network
 import Main as prev_main
 
-data_path = '../../niquo_data/July_first_week...csv'
+data_path = '../../niquo_data/July_first_week_random_walk.csv'
 destination_path = '../../niquo_data/simulation'
 graph_path = '../../niquo_data/simulation/simulation_graph.p'
 
