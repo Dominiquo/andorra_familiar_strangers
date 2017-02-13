2d72af77537c26073797d187d2698b4254e6fe6d
Niquo <niquo@mit.edu>
2017-02-13 15:27:11 -0500

    WIP adding dataframe to partitioning of data

--- niquo_work/Misc/file_constants.py
+++ niquo_work/Misc/file_constants.py
@@ -1,8 +1,8 @@
-JULY_DATA_ALL = '../../data_repository/datasets/telecom/cdr/201607-AndorraTelecom-CDR.csv'
-JULY_DATA_FILTERED = '../niquo_data/filtered_data/06_2017_no_data.csv'
-TOWERS_ID = '../../../workspace/yleng/towers1.csv'
-TOWERS_TYPE = '../../niquo_data/20151120-towersContainerTrip.csv'
-FILTERED_PARTITIONED = '../niquo_data/filtered_data/partitioned_towers/'
+JULY_DATA_ALL = '../../../data_repository/datasets/telecom/cdr/201607-AndorraTelecom-CDR.csv'
+JULY_DATA_FILTERED = '../../niquo_data/filtered_data/06_2017_no_data.csv'
+TOWERS_ID = '../../../../workspace/yleng/towers1.csv'
+TOWERS_TYPE = '../../../niquo_data/20151120-towersContainerTrip.csv'
+FILTERED_PARTITIONED = '../../niquo_data/filtered_data/partitioned_towers/'
 
 '''	1 = shoppping
 	2 = nature interests
--- niquo_work/Structures/RawCRDData.py
+++ niquo_work/Structures/RawCRDData.py
@@ -27,57 +27,68 @@ class RawCDRCSV(object):
 	def __init__(self, filename):
 		self.filename = filename
 
-	def filter_and_partition(self,destination_dir, filter_func=lambda row: True, limit=float('inf')):
+	def filter_and_partition(self, destination_dir, filter_func=lambda row: True, chunksize=10**4):
 		tower_map = Maps.tower_map_id()
-		tower_file_prefix = "cdr_tower_"
-		csv_suffix = ".csv"
-		current_towers = set([])
-		current_count = 0
-		files_count = 1
-		total_ids = set([])
+		tower_file_prefix = 'cdr_tower_'
+		csv_suffix = '.csv'
+		
 
-		for row in self.rows_generator():
-			if (row[TOWER_INDEX]== 'ID_CELLA_INI') or (not filter_func(row)):
-				continue
-			pre_funnel_id = row[TOWER_INDEX]
-			if pre_funnel_id not in tower_map:
-				continue
-			else:
-				tower_id = tower_map[pre_funnel_id]
 
-			if tower_id not in total_ids:
-				total_ids.add(tower_id)
-			call_time = row[START_TIME_INDEX]
-			call_date = call_time[:DATE_INDEX]
-			date_path = os.path.join(destination_dir,call_date)
-			# check if the path for the date exists yet
-			if not os.path.exists(date_path):
-				os.makedirs(date_path)
 
-			tower_file = tower_file_prefix + tower_id + csv_suffix
-			tower_path = os.path.join(date_path,tower_file)
-			if tower_path in current_towers:
-				tower_file_obj = open(tower_path, 'a')
-			else:	
-				files_count += 1
-				tower_file_obj = open(tower_path,'wb')
 
-			tower_file_csv = csv.writer(tower_file_obj,delimiter=';')
-			tower_file_csv.writerow(row)
-			tower_file_obj.close()
 
-			if current_count > limit:
-				break
-			current_count += 1
-			current_towers.add(tower_path)			
-		print 'created',files_count,'new files of towers.'
+	# def filter_and_partition(self,destination_dir, filter_func=lambda row: True, limit=float('inf')):
 
-	def rows_generator(self):
-		print 'opening file to read from as a csv...'
-		with open(self.filename) as csvfile:
-			data_csv = csv.reader(csvfile,delimiter=',')
-			for row in data_csv:
-				yield row
+	# 	tower_map = Maps.tower_map_id()
+	# 	tower_file_prefix = "cdr_tower_"
+	# 	csv_suffix = ".csv"
+	# 	current_towers = set([])
+	# 	current_count = 0
+	# 	files_count = 1
+	# 	total_ids = set([])
+
+	# 	for row in self.rows_generator():
+	# 		if (row[TOWER_INDEX]== 'ID_CELLA_INI') or (not filter_func(row)):
+	# 			continue
+	# 		pre_funnel_id = row[TOWER_INDEX]
+	# 		if pre_funnel_id not in tower_map:
+	# 			continue
+	# 		else:
+	# 			tower_id = tower_map[pre_funnel_id]
+
+	# 		if tower_id not in total_ids:
+	# 			total_ids.add(tower_id)
+	# 		call_time = row[START_TIME_INDEX]
+	# 		call_date = call_time[:DATE_INDEX]
+	# 		date_path = os.path.join(destination_dir,call_date)
+	# 		# check if the path for the date exists yet
+	# 		if not os.path.exists(date_path):
+	# 			os.makedirs(date_path)
+
+	# 		tower_file = tower_file_prefix + tower_id + csv_suffix
+	# 		tower_path = os.path.join(date_path,tower_file)
+	# 		if tower_path in current_towers:
+	# 			tower_file_obj = open(tower_path, 'a')
+	# 		else:	
+	# 			files_count += 1
+	# 			tower_file_obj = open(tower_path,'wb')
+
+	# 		tower_file_csv = csv.writer(tower_file_obj,delimiter=';')
+	# 		tower_file_csv.writerow(row)
+	# 		tower_file_obj.close()
+
+	# 		if current_count > limit:
+	# 			break
+	# 		current_count += 1
+	# 		current_towers.add(tower_path)			
+	# 	print 'created',files_count,'new files of towers.'
+
+	# def rows_generator(self):
+	# 	print 'opening file to read from as a csv...'
+	# 	with open(self.filename) as csvfile:
+	# 		data_csv = csv.reader(csvfile,delimiter=',')
+	# 		for row in data_csv:
+	# 			yield row
 
 
 
