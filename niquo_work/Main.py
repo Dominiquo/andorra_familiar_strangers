e2c87019dbdea0babd32eade47d1bfc0209a347d
Niquo <niquo@mit.edu>
2017-02-24 11:44:25 -0500

    fixed variable name change

--- niquo_work/Main.py
+++ niquo_work/Main.py
@@ -4,12 +4,12 @@ import Structures.RawCRDData as raw
 import Structures.TowersPartitioned as TP
 
 def Main(root_path = '../niquo_data/spring_data/'):
-	
+
 	partitioned_data_path = os.path.join(root_path, 'partitioned_data')
 	tower_enc_path = os.path.join(root_path, 'tower_encounters/')
 
-	tpart = TP.TowersPartitioned(data_path)
-	tpart.pair_users_from_towers(dest_path)
+	tpart = TP.TowersPartitioned(partitioned_data_path)
+	tpart.pair_users_from_towers(tower_enc_path)
 	return True
 
 
