6c7a8e330bcfe164d9ebb453b93a513e6cae11a9
Niquo <niquo@mit.edu>
2017-02-24 14:45:05 -0500

    run the data for multiple days on Main

--- niquo_work/Main.py
+++ niquo_work/Main.py
@@ -9,7 +9,8 @@ def Main(root_path = '../niquo_data/spring_data/'):
 	tower_enc_path = os.path.join(root_path, 'tower_encounters/')
 
 	tpart = TP.TowersPartitioned(partitioned_data_path)
-	tpart.pair_users_from_towers(tower_enc_path)
+	# tpart.pair_users_from_towers(tower_enc_path)
+	tpart.pair_towers_multiple_days(dest_path)
 	return True
 
 
