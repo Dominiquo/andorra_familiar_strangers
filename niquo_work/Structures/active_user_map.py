c3071f203d674a691020ded9b735cfd86b1d0486
Niquo <niquo@mit.edu>
2017-04-14 14:27:45 -0400

    add print statements and return graph

--- niquo_work/Structures/active_user_map.py
+++ niquo_work/Structures/active_user_map.py
@@ -12,16 +12,22 @@ import time
 
 
 def get_active_users(data_path, lower_range=0, upper_range=float('inf')):
+	print 'loading coompressed data...'
 	df = pd.read_csv(data_path)
+	print 'grouping by SOURCE'
 	grouped = df.groupby(constants.SOURCE).size().sort_values()
+	print 'getting valid values'
 	valid_values = grouped[grouped.between(lower_range, upper_range, inclusive=True)]
 	return set(valid_values.index)
 
 
 def reduce_size(graph_path, data_path, lower_range, upper_range, destination_path):
 	with open(graph_path, 'rb') as infile:
+		print 'loading graph object'
 		G = cPickle.load(infile)
 	active_users = get_active_users(data_path, lower_range, upper_range)
+	print 'removing nodes...'
 	for node in G.nodes():
 		if node not in active_users:
 			G.remove_node(node)
+	return G
