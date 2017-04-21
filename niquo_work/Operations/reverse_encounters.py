e9ff4374712c0db15628abdc49d27fa9236e89d4
Niquo <niquo@mit.edu>
2017-04-21 10:51:29 -0400

    fixed indentation

--- niquo_work/Operations/reverse_encounters.py
+++ niquo_work/Operations/reverse_encounters.py
@@ -9,11 +9,11 @@ USE_MONTHS = ['201507-AndorraTelecom-CDR.csv',
  '201512-AndorraTelecom-CDR.csv']
 
 
- def get_new_friend_set(new_friend_csv):
- 	friend_df = pd.read_csv(new_friend_csv)
- 	first_col = friend_df.columns[0]
- 	split_to_set = lambda tied_val: set{tied_val.split('_')}
- 	user_set = set([])
- 	for val in first_col.values:
- 		user_set = user_set.union(split_to_set(val))
- 	return user_set
+def get_new_friend_set(new_friend_csv):
+	friend_df = pd.read_csv(new_friend_csv)
+	first_col = friend_df.columns[0]
+	split_to_set = lambda tied_val: set{tied_val.split('_')}
+	user_set = set([])
+	for val in first_col.values:
+		user_set = user_set.union(split_to_set(val))
+	return user_set
