86f092b8647bf112c6f0624e1a313adf3b6fafa8
Niquo <niquo@mit.edu>
2017-05-05 17:55:31 -0400

    check if path exists

--- niquo_work/Social/graph_encounters.py
+++ niquo_work/Social/graph_encounters.py
@@ -68,24 +68,28 @@ def get_social_encounters(social_path_0, social_path_1, social_path_2, encounter
 		df_dict[constants.USER_1].append(source)
 		df_dict[constants.USER_2].append(dest)
 		df_dict[constants.ENCS_COUNT].append(encs_count)
-		distance_0, _ = nx.path.bidirectional_dijkstra(social_graph_0, source, dest)
-		distance_1, _ = nx.path.bidirectional_dijkstra(social_graph_1, source, dest)
-		distance_2, _ = nx.path.bidirectional_dijkstra(social_graph_2, source, dest)
-		if distance_0:
+		has_distance_0 = nx.has_path(social_graph_0, source, dest)
+		has_distance_1 = nx.has_path(social_graph_1, source, dest)
+		has_distance_2 = nx.has_path(social_graph_2, source, dest)
+
+		if has_distance_0:
+			distance_0 = nx.shortest_path_length(social_graph_0, source, dest)
 			df_dict[constants.MODE_0_DIST].append(distance_0)
 		else:
 			df_dict[constants.MODE_0_DIST].append(NO_PATH)
 
-		if distance_1:
+		if has_distance_1:
+			distance_1 = nx.shortest_path_length(social_graph_1, source, dest)
 			df_dict[constants.MODE_1_DIST].append(distance_1)
 		else:
 			df_dict[constants.MODE_1_DIST].append(NO_PATH)
 
-		if distance_2:
+		if has_distance_2:
+			distance_2 = nx.shortest_path_length(social_graph_2, source, dest)
 			df_dict[constants.MODE_2_DIST].append(distance_2)
 		else:
 			df_dict[constants.MODE_2_DIST].append(NO_PATH)
-			
+
 	print 'edges without a social connection:', disconnected_edges
 	dataframe = pd.DataFrame(df_dict)
 	if save_file:
